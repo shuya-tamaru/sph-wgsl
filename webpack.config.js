fd9329e2066636963dfaf55829944480fa3afb40
shuya-tamaru <tamaru1013@gmail.com>
2025-08-24 14:01:59 +0900

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
+    publicPath: "/", // ルート配信なら "/"
+    clean: true,
   },
 
   module: {
