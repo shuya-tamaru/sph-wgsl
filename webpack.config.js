c0a6a41996318e52f52f18146ab7bb60f2d71c38
shuya-tamaru <tamaru1013@gmail.com>
2025-08-24 14:06:58 +0900

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
@@ -32,6 +33,12 @@ module.exports = {
   resolve: {
     extensions: [".ts"],
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html", // プロジェクトのルートにあるindex.htmlをテンプレートとして指定
+    }),
+  ],
 
   // 開発サーバーの設定
   devServer: !isProd
